bb4c945ff99772f31289eecd2c742eb195cdbee1
Frank <fbelanger@ackroo.com>
2018-08-19 12:22:57 -0400

    prepare for Heroku  deploy

---
+++ app/client/packs/cable.js
@@ -0,0 +1,11 @@
+import cable from "actioncable";
+
+let consumer;
+
+function createChannel(...args) {
+  if (!consumer) consumer = cable.createConsumer();
+
+  return consumer.subscriptions.create(...args);
+}
+
+export default createChannel;
