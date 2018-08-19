294f8bdb8a0f30cb811b1021fc677a2552db773c
Frank <fbelanger@ackroo.com>
2018-08-19 11:41:08 -0400

    part 2

---
+++ app/client/packs/components/messages/messages.js
@@ -0,0 +1,2 @@
+import "../message/message"; // message is nested, so we import it here
+import "./messages.css";
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root to: "page#home"
+  root to: "chat#show"
+
+  get  "/login", to: "auth#new"
+  post "/login", to: "auth#create"
 end
