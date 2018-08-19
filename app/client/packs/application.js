85ad38e99b5d16aa9a56d24d4acc88bfd89e5b5d
Frank <fbelanger@ackroo.com>
2018-08-19 13:00:21 -0400

    add back turbolinks and rails-ujs through yarn

--- app/client/packs/application.js
+++ app/client/packs/application.js
@@ -1,4 +1,15 @@
+// In order to re-enable Turbolinks in your project: yarn add turbolinks
+import Turbolinks from "turbolinks";
+
+// Without Sprockets, Rails would not understand Unobtrusive JavaScript
+// (that includes setting method: :delete on link_to)
+// unless you reinstall rails-ujs from npm: yarn add rails-ujs
+import Rails from "rails-ujs";
+
 import "./init";
 import "./components/page/page";
 import "./components/auth-form/auth-form";
 import "./components/chat/chat";
+
+Turbolinks.start();
+Rails.start();
